8c14e901e1fb589bbbf9fe6817321112fc7f6bc1
WarlOrd <tycoon111222333@gmail.com>
2025-02-25 16:45:04 +0300

    fix chrome.nix

--- home/programs/browsers/chrome.nix
+++ home/programs/browsers/chrome.nix
@@ -1,27 +1,9 @@
-
 {pkgs, ...}: {
-  programs.vivaldi = {
+  programs.google-chrome = {
     enable = true;
-    package = pkgs.vivaldi.override {
-      proprietaryCodecs = true;
-      enableWidevine = true;
-    };
     commandLineArgs = [
       "--enable-features=UseOzonePlatform"
       "--ozone-platform=wayland"
     ];
-    extensions = [
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm"
-      "kjhdffcfinhkdfbbhjlfoadcdfgihmlp"
-      "eimadpbcbfnmbkopoojfekhnkhdbieeh"
-      "apmmpaebfobifelkijhaljbmpcgbjbdo"
-      "cbghhgpcnddeihccjmnadmkaejncjndb"
-      "mnjggcdmjocbbbhaepdhchncahnbgone"
-      "pljikjgdcelenhecaainhheabilopmcc"
-    ];
   };
-
-  home.packages = with pkgs; [
-    vivaldi-ffmpeg-codecs
-  ];
 }
