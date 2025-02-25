06ed36d6ec16dbc7917469adbf011feaf4db7c0b
WarlOrd <tycoon111222333@gmail.com>
2025-02-25 16:43:21 +0300

    delete unused hosts

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,8 +1,6 @@
 {niksos, ...}: {
   flake.nixosConfigurations = with niksos.hosts;
     mkHosts {
-      table-niksne = systemProfiles.desktop;
       laptop-WarlOrd = systemProfiles.laptop;
-      niksos-wsl = systemProfiles.minimal;
     };
 }
