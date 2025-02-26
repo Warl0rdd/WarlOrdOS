19ddc46f52fc377180583b5009daeb339300cf31
WarlOrd <tycoon111222333@gmail.com>
2025-02-26 15:18:36 +0300

    add amneziaVPN client

--- system/network/amnezia.nix
+++ system/network/amnezia.nix
@@ -1,13 +1,4 @@
 {
-  config,
-  pkgs,
-  ...
-}: {
-  boot.extraModulePackages = with config.boot.kernelPackages; [
-    amneziawg
-  ];
-
-  environment.systemPackages = with pkgs; [
-    amneziawg-tools
-  ];
+    programs.amnezia-vpn.enable = true;
 }
+
